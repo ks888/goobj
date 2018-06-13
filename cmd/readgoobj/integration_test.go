e4ea5e16ef6907242b0fb72be776ea0686eb2fba
Kishin Yagami <k.yagami.suou@gmail.com>
2018-06-14 00:31:20 +0900

    Fix integration test

--- cmd/readgoobj/integration_test.go
+++ cmd/readgoobj/integration_test.go
@@ -22,37 +22,38 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+// Use already compiled program as an input because the object file is a little different by underlying OS and CPU.
 var programList = []struct {
 	name, expected string
 }{
 	{
-		name: "helloworld.go",
+		name: filepath.Join(testDataDir, "helloworld.o"),
 		expected: `The list of defined symbols:
  Offset Size Type        DupOK Local MakeTypeLink Name                                       Version GoType
- 0x0    0x6e STEXT       false false false        "".main                                    0
- 0x8d   0x5b STEXT       false false false        "".init                                    0
- 0x103  0x11 SRODATA     true  true  false        go.string."Hello, playground"              0
- 0x114  0x21 SDWARFINFO  false false false        go.info."".main                            0
- 0x135  0x0  SDWARFRANGE false false false        go.range."".main                           0
- 0x135  0x21 SDWARFINFO  false false false        go.info."".init                            0
- 0x156  0x0  SDWARFRANGE false false false        go.range."".init                           0
- 0x156  0x10 SRODATA     false false false        "".statictmp_0                             0       type.string
- 0x166  0x1  SNOPTRBSS   false false false        "".initdone·                              0       type.uint8
- 0x166  0x1  SRODATA     true  true  false        runtime.gcbits.01                          0
- 0x167  0x10 SRODATA     true  false false        type..namedata.*interface {}-              0
- 0x177  0x38 SRODATA     true  false true         type.*interface {}                         0
- 0x1af  0x1  SRODATA     true  true  false        runtime.gcbits.03                          0
- 0x1b0  0x50 SRODATA     true  false false        type.interface {}                          0
- 0x200  0x12 SRODATA     true  false false        type..namedata.*[]interface {}-            0
- 0x212  0x38 SRODATA     true  false true         type.*[]interface {}                       0
- 0x24a  0x38 SRODATA     true  false true         type.[]interface {}                        0
- 0x282  0x13 SRODATA     true  false false        type..namedata.*[1]interface {}-           0
- 0x295  0x38 SRODATA     true  false true         type.*[1]interface {}                      0
- 0x2cd  0x48 SRODATA     true  false true         type.[1]interface {}                       0
- 0x315  0x6  SRODATA     true  false false        type..importpath.fmt.                      0
- 0x31b  0x8  SRODATA     true  false false        gclocals·69c1753bd5f81501d95132d08af04464 0
- 0x323  0xa  SRODATA     true  false false        gclocals·e226d4ae4a7cad8835311c6a4683c14f 0
- 0x32d  0x8  SRODATA     true  false false        gclocals·33cdeccccebe80329f1fdbee7f5874cb 0`},
+ 0x3db  0x6e STEXT       false false false        "".main                                    0
+ 0x468  0x5b STEXT       false false false        "".init                                    0
+ 0x4de  0x11 SRODATA     true  true  false        go.string."Hello, playground"              0
+ 0x4ef  0x21 SDWARFINFO  false false false        go.info."".main                            0
+ 0x510  0x0  SDWARFRANGE false false false        go.range."".main                           0
+ 0x510  0x21 SDWARFINFO  false false false        go.info."".init                            0
+ 0x531  0x0  SDWARFRANGE false false false        go.range."".init                           0
+ 0x531  0x10 SRODATA     false false false        "".statictmp_0                             0       type.string
+ 0x541  0x1  SNOPTRBSS   false false false        "".initdone·                              0       type.uint8
+ 0x541  0x1  SRODATA     true  true  false        runtime.gcbits.01                          0
+ 0x542  0x10 SRODATA     true  false false        type..namedata.*interface {}-              0
+ 0x552  0x38 SRODATA     true  false true         type.*interface {}                         0
+ 0x58a  0x1  SRODATA     true  true  false        runtime.gcbits.03                          0
+ 0x58b  0x50 SRODATA     true  false false        type.interface {}                          0
+ 0x5db  0x12 SRODATA     true  false false        type..namedata.*[]interface {}-            0
+ 0x5ed  0x38 SRODATA     true  false true         type.*[]interface {}                       0
+ 0x625  0x38 SRODATA     true  false true         type.[]interface {}                        0
+ 0x65d  0x13 SRODATA     true  false false        type..namedata.*[1]interface {}-           0
+ 0x670  0x38 SRODATA     true  false true         type.*[1]interface {}                      0
+ 0x6a8  0x48 SRODATA     true  false true         type.[1]interface {}                       0
+ 0x6f0  0x6  SRODATA     true  false false        type..importpath.fmt.                      0
+ 0x6f6  0x8  SRODATA     true  false false        gclocals·69c1753bd5f81501d95132d08af04464 0
+ 0x6fe  0xa  SRODATA     true  false false        gclocals·e226d4ae4a7cad8835311c6a4683c14f 0
+ 0x708  0x8  SRODATA     true  false false        gclocals·33cdeccccebe80329f1fdbee7f5874cb 0`},
 }
 
 func TestSamplePrograms(t *testing.T) {
@@ -60,13 +61,7 @@ func TestSamplePrograms(t *testing.T) {
 	var cmdPath = filepath.Join(filepath.Dir(filename), "readgoobj")
 
 	for i, program := range programList {
-		goProgramName := filepath.Join(testDataDir, program.name)
-		objectFileName := filepath.Join(testDataDir, strings.Replace(program.name, ".go", ".o", 1))
-		if err := exec.Command("go", "tool", "compile", "-o", objectFileName, goProgramName).Run(); err != nil {
-			t.Fatalf("[%d] failed to compile go file: %+v", i, err)
-		}
-
-		out, err := exec.Command(cmdPath, objectFileName).CombinedOutput()
+		out, err := exec.Command(cmdPath, program.name).CombinedOutput()
 		if err != nil {
 			t.Fatalf("[%d] failed to run program\nerr: %v\nout: %v", i, err, string(out))
 		}
